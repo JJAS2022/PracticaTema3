680b90c480a577c9c02e3b29edcffa3653ee8f28
JAVIER JESUS ALIÑO SEHWERERT <javaliseh@alu.edu.gva.es>
2022-11-25 20:59:06 +0100

    Añadidos diseño y código del formulario.

--- Aplicación - PrácticaTema3/Aplicación - PrácticaTema3/Form1.Designer.cs
+++ Aplicación - PrácticaTema3/Aplicación - PrácticaTema3/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(29, 13);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(39, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Texto";
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbUrgente.Location = new System.Drawing.Point(35, 122);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(67, 17);
+            this.cbUrgente.TabIndex = 1;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPrecio.Location = new System.Drawing.Point(77, 153);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(34, 156);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(37, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Coste:";
+            // 
+            // btn1
+            // 
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Location = new System.Drawing.Point(210, 142);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(98, 38);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Calcular";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTelegrama.Location = new System.Drawing.Point(35, 29);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(273, 87);
+            this.txtTelegrama.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(348, 211);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Aplicación - PrácticaTema3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.CheckBox cbUrgente;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtTelegrama;
     }
 }
 
